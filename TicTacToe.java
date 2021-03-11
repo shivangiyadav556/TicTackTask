4d87ba49d4796471b14230f415cc2c264fc0ac25
Shivangi yadav <shivangiyadav556@gmail.com>
2021-03-11 23:45:00 +0530

    Adding hasconstantWonMathod

--- TicTacToe.java
+++ TicTacToe.java
@@ -57,6 +57,20 @@ public class TicTacToe {
 		return true;
 	}
 
-	
+	private static boolean hasContestantWon(char[][] board, char symbol) {
+		if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
+			(board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
+			(board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+			
+			(board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
+			(board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
+			(board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+			
+			(board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
+			(board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+			return true;
+		}
+		return false;
+	}
 	
 }
