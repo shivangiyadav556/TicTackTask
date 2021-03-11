418281f39911b33f6e5ace9727f7055f2a76403a
Shivangi yadav <shivangiyadav556@gmail.com>
2021-03-11 23:33:14 +0530

    initial commit

---
+++ TicTacToe.java
@@ -0,0 +1,37 @@
+package com.org.assignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		char[][] board = {{' ', ' ', ' '},
+				 	      {' ', ' ', ' '}, 
+				 	      {' ', ' ', ' '}};
+		
+		printBoard(board);
+		
+		while (true) {
+			playerTurn(board, scanner);
+			if (isGameFinished(board)){
+				break;
+			}
+			printBoard(board);
+			
+			computerTurn(board);
+			if (isGameFinished(board)){
+				break;
+			}
+			printBoard(board);
+		}
+		scanner.close();
+	}
+
+
+	
+	
+}
