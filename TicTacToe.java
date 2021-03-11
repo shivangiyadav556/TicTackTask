cbf1d3e5e46b39a60a24827ff6337b7516082f1d
Shivangi yadav <shivangiyadav556@gmail.com>
2021-03-12 00:07:47 +0530

    Adding printBoard

--- TicTacToe.java
+++ TicTacToe.java
@@ -154,5 +154,12 @@ private static void placeMove(char[][] board, String position, char symbol) {
 			default:
 				System.out.println(":(");
 		}
-
+private static void printBoard(char[][] board) {
+		System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
+		System.out.println("-+-+-");
+		System.out.println(board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] );
+		System.out.println("-+-+-");
+		System.out.println(board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] );
+	}
+	
 }
